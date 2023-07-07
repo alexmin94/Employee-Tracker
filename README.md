a2922972adeef0bf347a40de98da375f5a1ce44c
Alex <aminyakov94@gmail.com>
2023-07-07 18:35:32 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Employee-Tracker
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+
+  ## Description
+ With This Application, A User Can:
+
+- View all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role.
+- See department names and department id's.
+- View all roles' job title, the id, the department that role belongs to, and the salary for that role.
+- View a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to. 
+- View department information.
+- Add a role, an employee or a department.
+- Remove a role, an employee or a department.
+- View the total utilized budget of a department in other words and the combined salaries of all employees in that department.
+
+
+  ## Installation
+  To install dependencies, run:
+  ```
+  npm i
+  ```
+
+  ## Usage
+
+  - Install npm init -y to create a new .json file
+  
+  - Run npm i
+  - Run npm i inquirer
+  - Run npm i mysql
+  - Run node server.js
+  - Make sure server.js is connected to SQL before continuing
+  - Run through prompts as required
+
+  ## Contributing
+  No contributions needed
+
+  ## Questions
+  If you have any question, feel free to contact me directly at aminyakov94@gmail.com
+  or check out the rest of my work at [alexmin94](https://github.com/alexmin94) 
+
+  ## License
+  
+  My project is licensed with MIT license
--- server.js
+++ server.js
@@ -55,6 +55,7 @@ inquirer.prompt([
             break;
           case "addARole":
             addARole();
+            
             break;
           case "addAEmployee":
             addAEmployee();
@@ -75,7 +76,138 @@ inquirer.prompt([
         });
         
       }
-
+      function viewAllDepartments(){
+        db.query("SELECT * FROM department_list", function(err, results) {
+          (err) ? console.log(err) : console.table(results), promptUser();
+        });
+      }
+      
+      function viewAllRoles(){
+        db.query("SELECT * FROM role_list", function(err, results) {
+          (err) ? console.log(err) : console.table(results), promptUser();
+        })
+      }
+      function addADepartment() {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "addADepartment",
+            message: "enter a department dood."
+          }
+        ]).then((inquirerResponse) => {
+          console.log("department added:  " + inquirerResponse.addADepartment)
+          let departmentName = inquirerResponse.addADepartment
+          db.query(`INSERT INTO
+                    department_list 
+                    (dept_name) VALUES 
+                    ('${departmentName}')`, function(err, results){
+            (err) ? console.log(err) : console.table(`Added ${departmentName}!!!!`, results), promptUser()
+          })
+        }) 
+    }
+    
+    function addARole() {  
+      db.query("SELECT * FROM department_list", function(err, results) {
+        if (err) {
+          console.log(err);
+          return workTime();
+        }
+        const departmentChoices = results.map(department => ({
+          value: department.id,
+          name: department.dept_name
+        })); 
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "addARole",
+            message: "Enter a Role Dood."
+          },
+          {
+            type: "input",
+            name: "salary",
+            message: "how much this joker making?"
+          },
+          {    
+            type: "list",
+            name: "deptId",
+            message: "witch department does this belong to?",
+            choices: departmentChoices        
+          }
+         
+        ]).then((inquirerResponse)=> {
+            console.log("Role added:  " + inquirerResponse.addARole)
+            let departmentId = inquirerResponse.deptId;      
+            let roleName = inquirerResponse.addARole;
+            let roleSalary = inquirerResponse.salary;
+            db.query(`INSERT INTO 
+                     role_list
+                     (title, salary, department_list_id) 
+                     VALUES 
+                     ('${roleName}', 
+                    '${roleSalary}',
+                    '${departmentId}')`, function(err, results){
+              (err) ? console.log(err) : console.table(`Added:  ${roleName}!!!!`,results) , promptUser()
+            })
+          })    
+        });
+      
+    }
+    function addAEmployee() {
+        db.query("SELECT * FROM role_list", function (err, results) {
+          if (err) {
+            console.log(err);
+            return workTime();
+          }
+      
+          const roleChoices = results.map(role => ({
+            value: role.id,
+            name: role.title
+          }))
+          ///inquirer
+          inquirer.prompt([
+            {
+              type: "input",
+              name: "firstName",
+              message: "Enter an employee name dood."
+            },
+            {
+              type: "input",
+              name: "lastName",
+              message: "enter an employee last name dood."
+            },
+            {
+              type: "list",
+              name: "roleId",
+              message: "wich role are we adding this guy to.",
+              choices: roleChoices
+            }
+          ]).then((inquirerResponse) => {
+            console.log("dood added: " + inquirerResponse.roleId)
+            let roleId = inquirerResponse.roleId;
+            let empName = inquirerResponse.firstName;
+            let empLast = inquirerResponse.lastName;
+            db.query(`INSERT INTO employee_list 
+                     (first_name, last_name, 
+                      role_list_id) VALUES 
+                      ('${empName}', 
+                      '${empLast}', 
+                      '${roleId}')`, function (err, results){
+                        (err) ? console.log(err) : console.table(`Added:  ${empName}!!!!`,results) , promptUser()
+                      })
+                    })   
+                  });   
+                }
+      
+      function quit(){
+        console.log("quitting you")
+        process.exit()
+      };
+      
+      app.use((req, res) => {
+        res.status(404).end();
+        });
+      
+    
 
 
 app.listen(PORT, () => {
